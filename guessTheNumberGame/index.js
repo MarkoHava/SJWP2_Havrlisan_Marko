b2395602ab68dde348db7e37d7ee17d63a2d822c
MarkoHava <87781718+MarkoHava@users.noreply.github.com>
2025-11-25 12:00:08 +0100

    guess the number game

--- guessTheNumberGame/index.js
+++ guessTheNumberGame/index.js
@@ -10,14 +10,38 @@ let rnd = randomNumber();
 let pokusaji = 0;
 
 function updateAttempts(){
-attempts.innerHTML = "Pokušaji: " + pokusaji;
+    attempts.innerHTML = "Pokušaji: " + pokusaji;
 }
 updateAttempts();
 function CheckNumber(event){
-event.preventDefault();
-let value = parseInt(input.value);
-console.log(value);
+    event.preventDefault();
+    let value = parseInt(input.value);
+    //console.log(value);
+
+    if(isNaN(value) || value<1 || value>100){
+        msg.innerHTML  = "unesi broj izmedu 1 i 100";
+        msg.style.color = "crimson";
+        return;
+    }
+
+    if(value=== rnd){
+        msg.innerHTML = "pogodak. Broj je " + rnd;
+        msg.style.color="green";
+    }else if(value<rnd){
+        msg.innerHTML = "Pre malo"
+        msg.style.color="orange";
+    }else{
+        msg.innerHTML = "previse";
+        msg.style.color="orange";
+    }
+    pokusaji++;
+    updateAttempts();
 }
 function NewGame(){
-
+    rnd = randomNumber();
+    pokusaji=0;
+    updateAttempts();
+    msg.innerHTML="Nova igra - pokušaj ponovo";
+    msg.style.color="black";
+    input.focus();
 }
