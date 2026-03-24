17187b29967cf9088b20021088cdf9bd1ece9835
markohava <markohavrlisan@gmail.com>
2026-03-24 15:23:14 +0100

    zadaca1 updated

--- StranicaFilmovi/index.js
+++ StranicaFilmovi/index.js
@@ -1,7 +1,39 @@
+const SrcSerije = {}
+const SrcFilmovi = ['revengeofthesith', 'blackadam', 'godfather',
+    'hobbit-thedesolationofsmaug', 'hp-thephilosophersstone',
+    'lordoftherings-thefellowshipofthering', 'lordoftherings-thereturnoftheking',
+    'swnh', 'swsmece1', 'swsmece2', 'thedarkknight', 'hp-dhpt1', 'hp-dhpt2', 'hp-prisoner',
+    'jaws', 'svadba'
+]
+const TtlFilmovi = ['Star Wars: Revenge of the Sith', 'Black Adam', 'Godfather', 'Hobbit: The Desolaton of Smaug',
+    'Harry Potter and the Philosophers Stone', 'Lord of the Rings: The Fellowship of the Ring',
+    'Lord of the Rings: The Return of the King', 'Star Wars: a New Hope', 'Star Wars: The Force Awakens',
+    'Star Wars: The Last Jedi', 'The Dark Knight'
+]
+const SrcDokumentarci = {}
+
+
+
 function Serije(){
     var images = document.getElementsByTagName("img");
-    images.forEach(element => {
-        element.src = null;
-    });
+    for (let i = 0; i<images.length; i++){
+        images[i].src =  './Serije/' +  SrcSerije[i];
+    }
+    console.log("uspjeh");
+}
+function Filmovi(){
+    var images = document.getElementsByTagName("img");
+    var ttls = document.getElementsByClassName("eltitle");
+    for (let i = 0; i<images.length; i++){
+        images[i].src =  './Filmovi/' +  SrcFilmovi[i] + '.jpg';
+        ttls[i].innerHTML =TtlFilmovi[i];
+    }
+    console.log("uspjeh");
+}
+function Dokumentarci(){
+    var images = document.getElementsByTagName("img");
+    for (let i = 0; i<images.length; i++){
+        images[i].src = './Dokumentarci/' +  SrcDokumentarci[i];
+    }
     console.log("uspjeh");
 }
